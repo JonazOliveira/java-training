74a1f505fcf2f10284b9742ab78c535584ecbfe2
JonazOliveira <jonasdeoliveira91@gmail.com>
2024-07-26 11:43:19 -0300

    Code escope

---
+++ NumberToReversedArray.java
@@ -0,0 +1,16 @@
+/*
+    Given a random non-negative number,
+    have to return the digits of this number within an array in reverse order.
+ */
+/*
+    Example:
+        35231 => [1,3,2,5,3]
+        0 => [0]
+ */
+
+public class NumberToReversedArray {
+    public static int[] digitize(long number) {
+        int[] array = new int[1];
+        return array;
+    }
+}
