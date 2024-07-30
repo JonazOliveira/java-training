4d8e5e84856a293a936b3204ddff1499aa339a57
JonazOliveira <jonasdeoliveira91@gmail.com>
2024-07-30 20:49:26 -0300

    Escope of code

---
+++ SumTwoLowest.java
@@ -0,0 +1,17 @@
+/*
+    Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers.
+    No floats or non-positive integers will be passed.
+ */
+/*
+    Those integers will come as double precision (long).
+ */
+/*
+    Examples:
+        [19, 5, 42, 2, 77] => 7 (5+2)
+        [10, 343445353, 3453445, 3453545353453] => 3453455 (10+3453445)
+ */
+
+public class SumTwoLowest {
+    public static long sumTwoSmallestNumbers(final long [] numbers) {
+        return 0L;
+}
