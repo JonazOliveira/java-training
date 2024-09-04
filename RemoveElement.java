a4c017cf28d6b4e4c3f0fbca9bc31ea76887628b
JonazOliveira <jonasdeoliveira91@gmail.com>
2024-09-04 01:05:56 -0300

    Code Solved

--- RemoveElement.java
+++ RemoveElement.java
@@ -1,5 +1,26 @@
+/*
+    Change the array nums such that the first k elements of
+        nums contain the elements which are not equal to val and return k.
+
+    Constraints:
+        0 <= nums.length <= 100
+        0 <= nums[i] <= 50
+        0 <= val <= 100
+ */
+
 public class RemoveElement {
     public int removeElement(int[] nums, int val) {
-        return 0;
+        int k = nums.length;
+        int[] result = nums;
+
+        for(int i = 0, j = 0; i < nums.length; i++){
+            if(result[i] == val){
+                k--;
+            } else {
+                nums[j] = result[i];
+                j++;
+            }
+        }
+        return k;
     }
 }
