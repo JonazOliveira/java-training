81a17bd6f1ce43f610db122c4186c853ccd9c8d7
JonazOliveira <jonasdeoliveira91@gmail.com>
2024-09-06 00:07:27 -0300

    Escope code

---
+++ RemoveDuplicateSortedArray.java
@@ -0,0 +1,16 @@
+/*
+    Given an integer array nums sorted in non-decreasing order,
+        remove the duplicates in-place such that each unique element appears only once.
+    The relative order of the elements should be kept the same.
+
+    Constraints:
+        1 <= nums.length <= 3 * 104
+        -100 <= nums[i] <= 100
+        nums is sorted in non-decreasing order.
+ */
+
+public class RemoveDuplicateSortedArray {
+    public int removeDuplicates(int[] nums) {
+        return 0;
+    }
+}
